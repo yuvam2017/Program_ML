360c0601c69020173e64d7472c901099b4a3eec6
yuvam2017 <dryuvam14900@gmail.com>
2023-01-14 19:14:38 +0530

    Program1pdf

--- q1b.py
+++ q1b.py
@@ -1,24 +1,22 @@
-# (b) Perform other matrix operations:
-#      (i) convert matrix data to absolute values
-#      (ii) take the negative of matrix values
-#      (iii) add rows & columns from a matrix
-#      (iv) remove rows & columns from a matrix
-#      (v) find the maximum and  minimum values in a matrix or in a row/column
-#      (vi) find the sum of some/all elements in a matrix
 import numpy as np
 
 def manipulateMatrix(matrix):
-    print("######### Absolute Matrix #########\n",np.abs(matrix))
+    print("##### Absolute Matrix #####\n",np.abs(matrix))
     
-    print("######### Negative Values #########\n",matrix[np.where(matrix<0)])
+    print("###### Negative Values #####\n",matrix[np.where(matrix<0)])
     
     addColAndRow(matrix)
-
     removeColAndRow(matrix,2,3)
+    matrix = np.abs(matrix)
+    maximum,minimum = np.max(matrix),np.min(matrix)
+    print("Maximum value in absolute matrix ==> ",maximum)
+    print("Minimum value in absolute matrix ==> ",minimum)
+
+    print("Sum of all elements in absolute matrix ==> ",np.sum(matrix))
 
 def addColAndRow(matrix):
     matrix2 = np.abs(np.round_(np.random.randn(5,6) * 10,decimals=2))
-    print("######### Matrix2 #########\n",matrix2)
+    print("##### Matrix2 #####\n",matrix2)
 
     x1,y1 = np.shape(matrix)
     x2,y2 = np.shape(matrix2)
@@ -31,17 +29,17 @@ def addColAndRow(matrix):
 
     # note last rows and columns are added to the matrix
 
-    print("######### New Matrix  #########\n",newmatrix)
+    print("##### New Matrix  #####\n",newmatrix)
 
 def removeColAndRow(matrix,row,col):
-    print("######### Matrix #########\n",matrix)
+    print("##### Matrix #####\n",matrix)
     newmatrix2 = np.delete(matrix,row,axis=0)
     newmatrix2 = np.delete(newmatrix2,col,axis=1)
-    print(f"######### Removing {row+1} row And {col+1} column #########\n",newmatrix2)
+    print(f"#### Removing {row+1} row And {col+1} column ####\n",newmatrix2)
 
 
 if __name__ == "__main__":
     # created a sample matrix rrounding off to 2 decimal places and then multiplying the values by 10
     matrix = np.round_(np.random.randn(5,5) * 10,decimals=2)
-    print("######### Matrix #########\n",matrix)
+    print("##### Matrix #####\n",matrix)
     manipulateMatrix(matrix)
